4b66bce2b2cff109832f2e2aee15ae976cbf1537
Niscolinx <munisco12@gmail.com>
2019-11-22 03:56:07 +0100

    update

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -1,129 +1,129 @@
 
-    class Node {
-        constructor(val) {
-            this.val = val
-            this.next = null
-        }
+class Node {
+    constructor(val) {
+        this.val = val
+        this.next = null
+    }
+}
+
+class SinglyLinkedList {
+
+    constructor() {
+        this.head = null
+        this.tail = null;
+        this.length = 0
     }
 
-    class SinglyLinkedList {
+    push(val) {
+        let newNode = new Node(val)
+        if (!this.head) {
+            this.head = newNode
+            this.tail = this.head
+        }
+        else {
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length++
+        return this
 
-        constructor() {
+    }
+
+    pop() {
+        let current = this.head
+        let newTail = current
+        if (!this.head) return undefined
+        while (current.next) {
+            newTail = current
+            current = current.next
+        }
+        this.tail = newTail
+        this.tail.next = null
+        this.length--
+        if (this.length === 0) {
+            this.tail = null
             this.head = null
-            this.tail = null;
-            this.length = 0
         }
+        return newTail
 
-        push(val) {
-            let newNode = new Node(val)
-            if (!this.head) {
-                this.head = newNode
-                this.tail = this.head
-            }
-            else {
-                this.tail.next = newNode
-                this.tail = newNode
-            }
-            this.length++
-            return this
+    }
 
+    shift() {
+        let head = this.head
+        if (!head) return undefined
+        let newHead = this.head.next
+        this.head = newHead
+        this.length--
+        if (this.length === 0) {
+            this.tail = null
         }
-
-        pop() {
-            let current = this.head
-            let newTail = current
-            if (!this.head) return undefined
-            while (current.next) {
-                newTail = current
-                current = current.next
-            }
-            this.tail = newTail
-            this.tail.next = null
-            this.length--
-            if (this.length === 0) {
-                this.tail = null
-                this.head = null
-            }
-            return newTail.val
-
+        return head
+    }
+    unShift(val) {
+        let newNode = new Node(val)
+        this.length++
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head
         }
+        else {
+            let currentHead = this.head
+            this.head = newNode
+            this.head.next = currentHead
+            return currentHead
+        }
+    }
+    get(index) {
+        if (index >= this.length || index < 0) return null
+        let count = 0;
+        let head = this.head
+        while (count !== index) {
+            count++
+            head = head.next
+        }
+        return head
+    }
 
-        shift() {
-            let head = this.head
-            if (!head) return undefined
-            let newHead = this.head.next
-            this.head = newHead
-            this.length--
-            if (this.length === 0) {
-                this.tail = null
-            }
-            return head
+    set(val, index) {
+        let newNode = new Node(val)
+        let idx = this.get(index)
+        if (!idx) {
+            return false
         }
-        unShift(val) {
-            let newNode = new Node(val)
-            this.length++
-            if (!this.head) {
-                this.head = newNode;
-                this.tail = this.head
-            }
-            else {
-                let currentHead = this.head
-                this.head = newNode
-                this.head.next = currentHead
-                return currentHead
-            }
-        }
-        get(index) {
-            if (index >= this.length || index < 0) return null
-            let count = 0;
-            let head = this.head
-            while (count !== index) {
-                count++
-                head = head.next
-            }
-            return head
-        }
-
-        set(val, index) {
-            let newNode = new Node(val)
-            let idx = this.get(index)
-            if (!idx) {
-                return false
-            }
-            else {
-                idx.val = newNode.val
-                return true
-            }
-        }
-
-        insert(val, index){
-            let newNode = new Node(val)
-            if(index < 0 || index > this.length) return false
-            if(index === this.length) return this.push(val)
-            if(index === 0) return this.unShift(val)
-            else{
-                let preNode = this.get(index - 1)
-                let nextNode = preNode.next
-                preNode.next = newNode
-                newNode.next = nextNode
-                this.length++
-            }
-            
+        else {
+            idx.val = newNode.val
+            return true
         }
     }
 
-    let list = new SinglyLinkedList()
+    insert(val, index) {
+        let newNode = new Node(val)
+        if (index < 0 || index > this.length) return false
+        if (index === this.length) return !!this.push(val)
+        if (index === 0) return !!this.unShift(val)
+        else {
+            let preNode = this.get(index - 1)
+            let nextNode = preNode.next
+            preNode.next = newNode
+            newNode.next = nextNode
+            this.length++
+        }
 
-    list.push('hello')
-    list.push('world')
-    list.push('The')
-    list.push('Lord\'s')
-    list.push('good')
-    list.push('74')
-    list.insert('yeah', 1)
-    // list.pop()
-    // list.pop()
-    // list.pop()
-    //console.log(list.get(2))
-    return list
+    }
+
+    remove(index) {
+        if(index < 0 || index >= this.length) return undefined
+        index === 0 ? this.shift() : null
+        index === this.length - 1 ? this.pop() : null
+        let preNode = this.get(index - 1)
+        let removeNode = preNode.next
+        preNode.next = removeNode.next
+        this.length--
+        return removeNode
+
+
+    }
+}
+
+let list = new SinglyLinkedList()
 
