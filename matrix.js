6016d6ada776249b5a66d998b970be872d328723
Niscolinx <munisco12@gmail.com>
2020-01-14 21:17:22 +0100

    4x4 matrix solved

--- matrix.js
+++ matrix.js
@@ -1,17 +1,28 @@
-let arr = Array.from({length: 4}, () => [])
+(() => {
+    //Create an inner array that has a length of 4
+    let arr = Array.from({ length: 4 }, () => [])
 
+    let temp = arr.length
 
-for(let i = 0; i < arr.length; ++i){
-    
-    for(let j = i + 1; j <= arr.length; j++){
-        if(j === 4) {
-           for(let k = j; k < j; k++){
-               arr[i].push(k)
-           }
-        }
+    let count;
+
+    for (let i = 0; i < arr.length; ++i) {
         
-        arr[i].push(j)
-    }
-}
+        for (let j = i + 1; j <= arr.length; j++) {
+            
+            arr[i].push(j)
+        }
+        //After the first loop is done, take count and countinue looping 
+        count = temp - arr[i].length
+        for(let k = 1; k < count + 1; k++){
 
-console.table(arr)
+            arr[i].push(k)
+        }
+    }
+    // Reduce it to a single array
+   let newArr =  arr.reduce((a, b) => {
+       return a.concat(b)
+    }, [])
+    
+    return newArr
+})()
