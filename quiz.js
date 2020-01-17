a6c745985ed17c9eb5fd918a7848875ba2dc4699
Niscolinx <munisco12@gmail.com>
2020-01-17 21:17:57 +0100

    Can be stored using an array or object

--- quiz.js
+++ quiz.js
@@ -1,3 +1,4 @@
+//Storing it with arrays
 (() => {
     let user = ['user1', 'user2']
     let number = {
@@ -6,23 +7,47 @@
         'c': 3,
         'd': 4
     }
-
-    let obj = {}
+    let arr = []
     let count = 0
     for (let key in number) {
 
-        if (obj[user[count]]) {
-            obj[user[count]] = obj[user[count]].concat([number[key]])
-        }
-        else {
-            obj[user[count]] = [number[key]]
-        }
+        arr.push(`The user is ${user[count]} chose ${number[key]}`)
         count++
         if (count === user.length) {
             count = 0
         }
-
+        // for(let i = 0; i < count; i++){
+        //     count++
+        // }
     }
 
-    console.table(obj)
+    console.table(arr)
 })()
+
+//Storing it with objects
+let user = ['user1', 'user2']
+let number = {
+    'a': 1,
+    'b': 2,
+    'c': 3,
+    'd': 4
+}
+
+let obj = {}
+let count = 0
+for (let key in number) {
+
+    if (obj[user[count]]) {
+        obj[user[count]] = obj[user[count]].concat([number[key]])
+    }
+    else {
+        obj[user[count]] = [number[key]]
+    }
+    count++
+    if (count === user.length) {
+        count = 0
+    }
+
+}
+
+console.table(obj)
