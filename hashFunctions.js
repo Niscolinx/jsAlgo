e1013efc2d9bffec4ab96e9e1f64fedbf76e5365
Niscolinx <munisco12@gmail.com>
2020-01-18 19:57:37 +0100

    implemented the set method of hashFunctions

--- hashFunctions.js
+++ hashFunctions.js
@@ -1,13 +1,30 @@
-const hashFunctions = (str, length) =>{
-
-    let total = 0;
-    let primeNum = 31;
-    for(let i = 0; i < Math.min(str.length, 100); i++){
-        let val = str[i].charCodeAt(0) - 96
-        total += (total * primeNum + val) % length
+class hashTable {
+    constructor(size = 53) {
+        this.keyMap = Array.from({ length: size }, () => [])
     }
-    console.log(total)
+    _hash = (str) => {
+        let total = 0;
+        let primeNum = 31;
+        for (let i = 0; i < Math.min(str.length, 100); i++) {
+            let val = str[i].charCodeAt(0) - 96
+            total = (total * primeNum + val) % this.keyMap.length
+        }
+        return total
+    }
+
+    set = (key, val) => {
+        let num = this._hash(key)
+        if (this.keyMap[num]) {
+            this.keyMap[num].push([key, val])
+        }
+        else {
+         this.keyMap.push([key, val]);
+        }
+        console.table(this.keyMap)
+    }
 }
 
 
-hashFunctions('blue', 13)
+
+let hash = new hashTable()
+hash.set('salmon', 5)
