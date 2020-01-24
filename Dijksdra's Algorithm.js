39d94d1f69967d4235747d94a624eac1ca488149
Niscolinx <munisco12@gmail.com>
2020-01-25 00:28:11 +0100

    Documented the Dijksdra's algorithm since this is the first algorithm I solved without a pseudocode

--- Dijksdra's Algorithm.js
+++ Dijksdra's Algorithm.js
@@ -19,7 +19,7 @@ class graph {
     }
 
 
-
+    //Finds the smallest edge to N
     isSmaller(num) {
         for (let i in this.shortest) {
             if (!this.visited[i]) {
@@ -31,12 +31,15 @@ class graph {
     findShortestPath(v1, v2) {
 
         let smallest = Infinity;
-
+        
+        //Loops over the list and stores the edges in objects
         for (let key in this.list) {
             this.prev[key] = null
 
             if (key === v1) {
                 this.shortest[v1] = 0
+
+                //Gets the starting edge so that the checkprev function can you it for its logic
                 this.start = key
             }
             else {
@@ -44,33 +47,34 @@ class graph {
             }
         }
 
+        //Gets the smallest vertex to n, which should be the first edge in the shortest object (since  moving from n to n is zero)
         for (let i in this.shortest) {
             if (this.shortest[i] < smallest) {
                 smallest = this.shortest[i]
             }
         }
         for (let j in this.shortest) {
-            // if (this.visited[v2]) {
-                //     console.log(this.prev[v2])
-                //     return this.prev[v2]
-                // }
+          
                 let small = Infinity
-                
+
+                // generates a new edge to look into and find the paths linked to it
                 let key = this.isSmaller(smallest)
                 
                 if (this.visited[key]) return null
                 
             this.visited[key] = true
             
-            let checkWeight = Infinity
-            let newSum = 0;
+            let AllVisited = 0;
             for (let i = 0; i < this.list[key].length; i++) {
                 
+                //If the edge has not been visited, check it's corresponding vertices
                 if (!this.visited[this.list[key][i].node]) {
 
                     newSum++
                     let sum = this.vertexPath(key, this.list[key][i].node)
-                    
+
+                    //If  the previous sum of the vertices is not equal to the current sum(that means their paths are the same, so it's not shorter or longer),  added the shortest distance and update the previous path
+
                     if (this.shortest[this.list[key][i].node] !== sum) {
 
                         this.shortest[this.list[key][i].node] = sum
@@ -80,8 +84,9 @@ class graph {
                 }
             }
 
-            console.log(newSum)
-            if(newSum === 0){
+            //If every edge of the destination has been visited, that means the shortest path is now known
+
+            if(AllVisited === 0){
                  console.log('the shortest path is', this.prev[v2])
                  return this.prev[v2]
                 
@@ -101,7 +106,7 @@ class graph {
 
         }
     }
-
+    //Calculates the distance between two edges
     distance(x, y) {
 
         if (!y) return 0
@@ -130,13 +135,16 @@ class graph {
             arr.push([x, y])
             checkPrev(x)
 
+            //When Checkprev is done, total takes the sum of all the vertices from point n to point m
             let total = 0
             for (let i = 0; i < arr.length; i++) {
+                //distance is called as far as the path from point n to point m is not yet done
                 total += this.distance(arr[i][0], arr[i][1])
             }
             return total
         }
 
+        //Loops through the previous Edges to determine the path it took from point n to  point m and stores it in an array
         function checkPrev(edge) {
             let key = start
 
@@ -176,5 +184,5 @@ D.addEdge('C', 'F', 4)
 D.addEdge('D', 'E', 3)
 D.addEdge('D', 'F', 1)
 D.addEdge('F', 'E', 1)
-D.findShortestPath('A', 'E')
+D.findShortestPath('E', 'C')
 
