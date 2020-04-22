b4b7b2b5c1c1d23e82ab4d4af6e0de0ac7565d3c
Niscolinx <munisco12@gmail.com>
2020-04-22 08:05:19 +0100

    introduced the split keyword

--- JadenCasingStr.js
+++ JadenCasingStr.js
@@ -1,9 +1,6 @@
 String.prototype.toJadenCase = function () {
     //...
-    for (let i = 0; i < String.prototype.length; i++) {
-        console.log(String[i])
-    }
-    console.log('how are you doing')
+   console.log(this.split(''))
 };
 
 
