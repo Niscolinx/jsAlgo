f88d67b969af66552a62e4ec53b77b8a6f888337
Niscolinx <munisco12@gmail.com>
2020-04-22 07:42:33 +0100

    updated Jaden

---
+++ JadenCasingStr.js
@@ -0,0 +1,8 @@
+String.prototype.toJadenCase = function () {
+    //...
+    for (let i = 0; i < String.prototype.length; i++) {
+        console.log(String[i])
+    }
+};
+
+console.log('how are you this morning')
