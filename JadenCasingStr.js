d5db070f03ca3527008ade6278fa5c0fcf2c77e4
Niscolinx <munisco12@gmail.com>
2020-04-22 07:45:01 +0100

    updated Jaden

--- JadenCasingStr.js
+++ JadenCasingStr.js
@@ -5,7 +5,7 @@ String.prototype.toJadenCase = function () {
     }
 };
 
-console.log('how are you this morning')
+console.log(String.prototype.toJadenCase())
 
 var str = "How can mirrors be real if our eyes aren't real";
 str.toJadenCase()
