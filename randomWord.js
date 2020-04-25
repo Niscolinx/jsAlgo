59b814b9d4023deaab639135539a31f4e5e9294a
Niscolinx <munisco12@gmail.com>
2020-04-25 17:00:21 +0100

    Solving pickrandom

---
+++ randomWord.js
@@ -0,0 +1,12 @@
+// Write a program that has a list of ten words, some of which have repeated letters and some
+// which don’t.Write a program that picks a random word from the list that does not have any
+// repeated letters.
+
+const pickRandomWord = () => {
+
+    //Crated a list of random words with some words having repeated letters
+    let words = ['yaah', 'hello', 'world', 'come', 'tomorrow', 'alright', 'gotten', 'chatted', 'clipped', 'see']
+
+
+    
+}
