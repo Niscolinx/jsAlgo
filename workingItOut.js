7963894391dfe426e24d61ddcd9943cc0e3175d1
Igboanugwo Collins <munisco12@gmail.com>
2022-07-09 08:59:35 +0100

    practicing

--- workingItOut.js
+++ workingItOut.js
@@ -5,56 +5,36 @@
 //     function charCount(str) {
 //         //write out a object that keeps track of the characters
 //         let characters = {};
-        
+
 //         //Write a loop that iterates over the characters
 //         for (let i = 0; i <= str.length; i++) {
-//             //you check if the letter/number has been added before, if it hasn't the add the number and give it a value of one 
+//             //you check if the letter/number has been added before, if it hasn't the add the number and give it a value of one
 //             let obj = str[i];
-            
+
 //             characters[obj] = ++characters[obj] || 1
 //         }
 //         return characters;
-        
-        
+
 //     }
-    
+
 //     console.log(charCount('this is h234appeninuuug right n..ow'))
 // })()
 
+;(() => {
+    function doStuff(text) {
+        console.log({ text })
+        const lowerCased = text.toLocaleLowerCase()
+        const words = lowerCased.split(' ')
 
-(() => {
-function doStuff(text) {
-    console.log({text})
-    const lowerCased = text.toLocaleLowerCase()
-    const words = lowerCased.split(' ')
-    words.reverse()
-
-
-    const trimmedWords = []
-    for (let i in words) {
-        trimmedWords.push(words[i].trim())
-    }
-
-    const longWords = []
-    for (let i in trimmedWords) {
-        if (trimmedWords[i].length > 5) {
-            longWords.push(trimmedWords[i])
+        const longWords = []
+        for (let i in words) {
+            if (words[i].length > 5) {
+                longWords.push(words[i])
+            }
         }
+
+        return longWords.reverse().join(', ')
     }
 
-    let result = ''
-    for (let i in longWords) {
-        result += longWords[i]
-        result += ', '
-    }
-
-    console.log({result}, {trimmedWords})
-    return result.slice(0, -2)
-   // return console.log(result.slice(0, -2))
-}
-
-console.log('hello', doStuff("heLLo, world, how is everything"))
+    console.log('result', doStuff('heLLoworld world how is everything'))
 })()
-
-
-
