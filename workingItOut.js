58418e1a559dedff7c098f93831c6dc3af05621c
Igboanugwo Collins <munisco12@gmail.com>
2022-07-09 07:55:48 +0100

    practicing

--- workingItOut.js
+++ workingItOut.js
@@ -1,25 +1,25 @@
 // Write a function which takes in a string and returns the number of each character on the string
 
-(() => {
+// (() => {
 
-    function charCount(str) {
-        //write out a object that keeps track of the characters
-        let characters = {};
+//     function charCount(str) {
+//         //write out a object that keeps track of the characters
+//         let characters = {};
         
-        //Write a loop that iterates over the characters
-        for (let i = 0; i <= str.length; i++) {
-            //you check if the letter/number has been added before, if it hasn't the add the number and give it a value of one 
-            let obj = str[i];
+//         //Write a loop that iterates over the characters
+//         for (let i = 0; i <= str.length; i++) {
+//             //you check if the letter/number has been added before, if it hasn't the add the number and give it a value of one 
+//             let obj = str[i];
             
-            characters[obj] = ++characters[obj] || 1
-        }
-        return characters;
+//             characters[obj] = ++characters[obj] || 1
+//         }
+//         return characters;
         
         
-    }
+//     }
     
-    console.log(charCount('this is h234appeninuuug right n..ow'))
-})()
+//     console.log(charCount('this is h234appeninuuug right n..ow'))
+// })()
 
 
 (() => {
@@ -46,10 +46,10 @@ function doStuff(text) {
         result += ', '
     }
 
-    return result.slice(0, -2)
+    return console.log(result.slice(0, -2))
 }
 
-console.log(doStuff('hello'))
+console.log('hello', doStuff('hello'))
 })()
 
 
