9e7add131ba699f79f2520c3b967e3837b3cc900
Igboanugwo Collins <munisco12@gmail.com>
2022-07-23 13:51:25 +0100

    working on diagonalDifference

--- hankerrank.js
+++ hankerrank.js
@@ -1,12 +1,21 @@
-(() => {
-function diagonalDifference(arr) {
-    let l_to_r = 0
-    let r_to_l = 0
-    for (let i = 0; i < arr.length; i++) {}
+;(() => {
+    function diagonalDifference(arr) {
+        let l_to_r = 0
+        let r_to_l = 0
+        for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i])
+            for (let j = 0; j < arr[i][j].length; j++) {
+                l_to_r += arr[i][j]
+                r_to_l += arr[i]
 
-    console.log("it is working")
-    return 0
-}
+                console.log(arr[i][j])
+            }
+        }
+    }
 
-return diagonalDifference([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    return diagonalDifference([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ])
 })()
