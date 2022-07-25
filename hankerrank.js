67f0aeb8c32eeed321f10422fa02e8999657d0c3
Igboanugwo Collins <munisco12@gmail.com>
2022-07-25 17:53:30 +0100

    working staircase algorithm

--- hankerrank.js
+++ hankerrank.js
@@ -59,13 +59,9 @@
         for (let i = 0; i < n; i++) {
             let str = ''
             for (let j = 0; j < n; j++) {
-                if (j < n - i - 1) {
-                    str += ' '
-                } else {
-                    str += '#'
-                }
+               console.log({i,j})
             }
-            console.log(str)
+            console.log({str})
         }
 
     }
