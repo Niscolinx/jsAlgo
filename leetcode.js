307c35c9afcfa2434a3adedb11699a124365d1ac
Igboanugwo Collins <munisco12@gmail.com>
2022-07-28 12:49:51 +0100

    solved monotonic array leetcode

--- leetcode.js
+++ leetcode.js
@@ -1,28 +1,19 @@
 //Leetcode questions
-(() => {
-
+;(() => {
     //An array is monotonic if it is either monotone increasing or monotone decreasing.
     //An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
     //Given an integer array nums, return true if the given array is monotonic, or false otherwise.
 
-    var isMonotonic =  function(nums) {
-       
-
-        let mono = ''
-        if(nums[0] > nums[nums.length - 1]) {
-            mono = 'decreasing'
-            console.log({mono})
-            for(let i = 0; i < nums.length; i++) {
-                if(nums[i] < nums[i + 1]){
+    var isMonotonic = function (nums) {
+        if (nums[0] > nums[nums.length - 1]) {
+            for (let i = 0; i < nums.length; i++) {
+                if (nums[i] < nums[i + 1]) {
                     return false
                 }
             }
 
             return true
-        }
-        else {
-            mono = 'increasing'
-            console.log({mono})
+        } else {
             for (let i = 0; i < nums.length; i++) {
                 if (nums[i] > nums[i + 1]) {
                     return false
@@ -31,9 +22,7 @@
 
             return true
         }
-
-  
     }
 
-    console.log(isMonotonic([3,3,3,0,4]))
+    console.log(isMonotonic([3, 3, 3, 0, 4]))
 })()
