656c303cf6dfea80b3997c92bab71f30c44dc59c
Igboanugwo Collins <munisco12@gmail.com>
2022-11-23 15:01:31 +0100

    solving algo

--- leetcode/findPivotIndex.js
+++ leetcode/findPivotIndex.js
@@ -15,10 +15,23 @@
 // Right sum = nums[4] + nums[5] = 5 + 6 = 11
 
 const pivotIndex = function (nums) {
-    const leftNode = 0
-    const rightNode = 0
-
-    
+    // Initialize total sum of the given array...
+    let totalSum = 0
+    // Traverse the elements and add them to store the totalSum...
+    for (let i = 0; i < nums.length; i++) {
+        totalSum += nums[i]
+    }
+    // Initialize 'leftsum' as sum of first i numbers, not including nums[i]...
+    let leftSum = 0
+    // Again traverse all the elements through the for loop and store the sum of i numbers from left to right...
+    for (let i = 0; i < nums.length; i++) {
+        // sum to the left == leftsum.
+        // sum to the right === totalSum - leftsum - nums[i]..
+        // check if leftsum == totalSum - leftsum - nums[i]...
+        if (leftSum * 2 == totalSum - nums[i]) return i // Return the pivot index...
+        leftSum += nums[i]
+    }
+    return -1 // If there is no index that satisfies the conditions in the problem statement...
 }
 
 console.log(pivotIndex([1, 7, 3, 6, 5, 6]))
