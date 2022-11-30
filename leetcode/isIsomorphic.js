673616d8b0fac9459df5a504e9c9f900622f2a9c
Igboanugwo Collins <munisco12@gmail.com>
2022-11-30 14:30:08 +0100

    solving algo

--- leetcode/isIsomorphic.js
+++ leetcode/isIsomorphic.js
@@ -3,3 +3,28 @@
 // Two strings s and t are isomorphic if the characters in s can be replaced to get t.
 
 // All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+const isIsomorphic = function (s, t) {
+    let firstString = {}
+    let secondString = {}
+    for (let i = 0; i < s.length; i++) {
+        if (firstString[s[i]]) {
+            firstString[s[i]] = [...firstString[s[i]], i]
+        }
+        else{
+            firstString[s[i]] = [i]
+        }
+    }
+    for (let i = 0; i < t.length; i++) {
+        if (store[s[i]]) {
+            store[s[i]] = [...store[s[i]], i]
+        }
+        else{
+            store[s[i]] = [i]
+        }
+    }
+
+    return Object.values(store)
+}
+
+console.log(isIsomorphic('egg', 'add'))
