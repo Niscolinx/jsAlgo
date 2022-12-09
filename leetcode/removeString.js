5fb5f6d42242d6d6f18de91819a4b4f95be2deee
Igboanugwo Collins <munisco12@gmail.com>
2022-12-09 17:10:58 +0100

    solved algo

---
+++ leetcode/removeString.js
@@ -0,0 +1,22 @@
+const removeString = (str1, str2) => {
+
+    const lowerCaseStr1 = str1.toLowerCase()
+    const lowerCaseStr2 = str2.toLowerCase()
+
+
+    if(lowerCaseStr1.length !== lowerCaseStr2.length){
+
+        const getFirstSixChar = lowerCaseStr2.slice(0, lowerCaseStr1.length)
+
+        return lowerCaseStr1.concat(getFirstSixChar)
+    }
+
+
+    return lowerCaseStr1.concat(lowerCaseStr2)
+
+
+}
+
+console.log(
+    removeString('Praise', 'Cosinssdf')
+)
