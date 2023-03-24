58c5edae3607608a5fe1459468644eee81874eb9
Igboanugwo Collins <munisco12@gmail.com>
2023-03-24 14:39:44 +0100

    some js algo

--- practice/q1.js
+++ practice/q1.js
@@ -10,7 +10,7 @@ const givenArr = [3, 5, 7, 8, 9, 9, 12, 17]
 const squareArrAndSort = (arr) => {
 
    // loop through the arr and square each item
-
+    const updatedArr = []
    for(let item of givenArr){
         const square = Math.sqrt(item)
     }
