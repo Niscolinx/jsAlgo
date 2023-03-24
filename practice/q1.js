7d0b694aac1676669539428c72c64c6a85f1e8fb
Igboanugwo Collins <munisco12@gmail.com>
2023-03-24 14:39:09 +0100

    some js algo

--- practice/q1.js
+++ practice/q1.js
@@ -1,20 +1,20 @@
 const givenArr = [3, 5, 7, 8, 9, 9, 12, 17]
 
-// //const squareArrAndSort = (arr) => {
-
-//     //loop through the arr and square each item
-
-//  //   for(let item of givenArr){
-//         const square = Math.sqrt(item)
-//     }
-
-//     //return the sorted arr
-
-// }
-
 //First solution
 // const updatedArr = givenArr
 //     .map((item) => {
 //         return Math.pow(item, 2)
 //     })
 //     .sort((a, b) => a - b)
+
+const squareArrAndSort = (arr) => {
+
+   // loop through the arr and square each item
+
+   for(let item of givenArr){
+        const square = Math.sqrt(item)
+    }
+
+   // return the sorted arr
+
+}
