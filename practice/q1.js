4dc38a9227c26ebb6dc1dd75e8337a0796204b29
Igboanugwo Collins <munisco12@gmail.com>
2023-03-25 09:34:54 +0100

    some js algo

--- practice/q1.js
+++ practice/q1.js
@@ -8,19 +8,25 @@ const givenArr = [5, 3, -1, 4, 2, 0]
 //     })
 //     .sort((a, b) => a - b)
 
-const squareArrAndSort = (arr) => {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            if (Math.abs(arr[j], 2) < Math.abs(arr[i], 2)) {
-                const current = Math.abs(arr[j], 2)
-                arr[j] = Math.abs(arr[i], 2)
-                arr[i] = current
-            }
-        }
+// const squareArrAndSort = (arr) => {
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = i + 1; j < arr.length; j++) {
+//             if (Math.abs(arr[j], 2) < Math.abs(arr[i], 2)) {
+//                 const current = Math.abs(arr[j], 2)
+//                 arr[j] = Math.abs(arr[i], 2)
+//                 arr[i] = current
+//             }
+//         }
 
-        arr[i] = Math.pow(arr[i], 2)
-    }
-    return arr
+//         arr[i] = Math.pow(arr[i], 2)
+//     }
+//     return arr
+// }
+
+
+const squareArrAndSort = () => {
+
+    
 }
 
 console.log(squareArrAndSort(givenArr))
