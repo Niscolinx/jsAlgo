fe51212d2c0ec8251c1c616b37687c8574086f0b
Igboanugwo Collins <munisco12@gmail.com>
2023-03-27 15:33:30 +0100

    working on monotonic

---
+++ practice/q2.js
@@ -0,0 +1,12 @@
+// Question2
+// An array is monotonic if it is either monotone increasing or monotone decreasing. An array is monotone increasing if all its elements fromt the left to right are non-decreasing. An array is monotone descreasing if all its elements from the left to the right are non-increasing. Given an integer array return true if the given array is monotone, or false otherwise
+
+
+const isMonotonic = () => {
+
+    //Check if the last item is greater or equal to the first element
+    
+    
+    //Check if the last item is less than or equal to the first element
+    //
+}
