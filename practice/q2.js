b03195c846a6b25889f3c6f2cd9a1ea0199a4e20
Igboanugwo Collins <munisco12@gmail.com>
2023-03-27 15:42:46 +0100

    working on monotonic

--- practice/q2.js
+++ practice/q2.js
@@ -2,20 +2,39 @@
 // An array is monotonic if it is either monotone increasing or monotone decreasing. An array is monotone increasing if all its elements fromt the left to right are non-decreasing. An array is monotone descreasing if all its elements from the left to the right are non-increasing. Given an integer array return true if the given array is monotone, or false otherwise
 
 const isMonotonic = (arr) => {
-    //Check if the last item is greater or equal to the first element
-    //Return true or false
 
+    
+    //Check if the last item is greater or equal to the first element
     const isIncreasingOrEqual = arr[arr.length - 1] >= arr[0]
 
     if (isIncreasingOrEqual) {
         //Loop through and check if all the items are increasing or equal
 
         for (let i = 0; i < arr.length; i++) {
-            
+            if (arr[i + 1] < arr[i]) {
+                return false
+            }
+
+            return true
         }
     } else {
-    }
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i + 1] > arr[i]) {
+                return false
+            }
 
-    //Check if the last item is less than or equal to the first element
-    //
+            return true
+        }
+    }
+    
 }
+
+const givenArr = [2,2,2,2,2]
+// const givenArr = [2,1,0,5,2]
+// const givenArr = [2,0,0,1,1]
+// const givenArr = [1,5,5,6,8,12,3,0]
+// const givenArr = [1,5,5,6,8,12]
+// const givenArr = [7,6,5,1,3,0]
+// const givenArr = [7,6,5,2,1]
+
+console.log(isMonotonic(givenArr))
