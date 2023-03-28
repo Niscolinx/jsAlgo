0ea361a1378c5b34ce268264101781707a803afd
Igboanugwo Collins <munisco12@gmail.com>
2023-03-28 09:31:36 +0100

    solving algo

--- practice/q4.js
+++ practice/q4.js
@@ -3,7 +3,6 @@
 
 // Find two lines that together with the x-axis form a container, such that the container contains the most water(depth is constant across containers). Return the area(that the 2 lines and the X axis make) of container which can store the max amount of water. Notice that you may not slant the container
 
-
 //Constraints
 //y axis does not count as a wall
 //A line inside the container does not affect the area
