30da1b927e902e4ae35e33bea113822efd7ec22d
Igboanugwo Collins <munisco12@gmail.com>
2023-03-28 10:36:57 +0100

    working on k rotations

--- practice/q3.js
+++ practice/q3.js
@@ -1,22 +1,18 @@
 // Question 3
 // Given an array, rotate the array to the right by k steps, where k is non-negative
 
-//Constraints 
+//Constraints
 //1. if arr = [] return []
 //2. if k = 0, no rotation
 
-
 //Solution
 
-const givenArr = [1,2,3,4]
-
+const givenArr = [1, 2, 3, 4]
 
 const rotateArr = (arr, k) => {
-
-    if(arr.length < 1) return []
-    if(k === 0) return arr
-    if(k > arr.length) return arr
-
+    if (arr.length < 1) return []
+    if (k === 0) return arr
+    if (k > arr.length) return arr
 
     //should return [4,3,2,1] for k = 1
     //should return [3,4,1,2] for k = 2
@@ -26,14 +22,15 @@ const rotateArr = (arr, k) => {
 
     //slice through k steps, starting from the end
 
-     const k_sliced = arr.slice(arr.length - 1, k)
-     const remaining_sliced = arr.slice(0, arr.length - k)
+    const k_sliced = arr.slice(arr.length - 1, k)
+    const remaining_sliced = arr.slice(0, arr.length - k)
 
-     //spread into the rotatedArr
+    console.log({k_sliced, remaining_sliced})
+
+    //spread into the rotatedArr
     rotatedArr = [...k_sliced, ...remaining_sliced]
 
     return rotatedArr
-
 }
 
-console.log(rotateArr(givenArr))
+console.log(rotateArr(givenArr, 1))
