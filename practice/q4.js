77e4d325a77f6379da945fa5e275e6a53b8a1a5f
Igboanugwo Collins <munisco12@gmail.com>
2023-03-30 09:27:39 +0100

    solving algo

---
+++ practice/q4.js
@@ -0,0 +1,9 @@
+// Question
+// You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith lines are (i, 0) and (i, height[i]).
+
+// Find two lines that together with the x-axis form a container, such that the container contains the most water(depth is constant across containers). Return the area(that the 2 lines and the X axis make) of container which can store the max amount of water. Notice that you may not slant the container
+
+
+//Constraints
+//y axis does not count as a wall
+//A line inside the container does not affect the area
