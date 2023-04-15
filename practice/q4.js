9526750af298713e39485c5285771757e86edce9
Igboanugwo Collins <munisco12@gmail.com>
2023-04-15 11:34:41 +0100

    working on water implementation

--- practice/q4.js
+++ practice/q4.js
@@ -127,4 +127,6 @@ console.log(mostWaterContainer1([2, 4, 1, 4, 7, 8, 5, 6]))
 //Get the difference between their indices = 6 - 0 = 6
 //Area will now be 2 * 6 = 12
 //get the area, then add one from the lower value
-//1. area = 14, moveCount = moveCount--
+//1. area = 2* 6 = 12, moveCount = moveCount--
+//2. area = 4* 6 = 24, moveCount = moveCount--
+//3. area = 1*5 = 24, moveCount = moveCount--
