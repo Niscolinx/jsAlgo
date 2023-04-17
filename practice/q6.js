c328dd207148f17230caad4947eac3b056c9947d
Igboanugwo Collins <munisco12@gmail.com>
2023-04-17 13:37:47 +0100

    working on algo

--- practice/q6.js
+++ practice/q6.js
@@ -16,18 +16,16 @@
 // }
 
 const isomorphic = (str1, str2) => {
-    const frq_1 = {}
-    const frq_2 = {}
+    const frq_1 = new Map()
+    const frq_2 = new Map()
 
     for (let i = 0; i < str1.length; i++) {
-        if (frq_1[str1[i]]) {
-
-            frq_1[str1[i]] = frq_1[str1[i]].concat(i)
-            frq_2[str2[i]] = frq_2[str2[i]].concat(i)
-
+        if (frq_1.has(str1[i])) {
+            frq_1.set(str1[i], frq_1.get(str1[i]).concat(i))
+            frq_2.set(str2[i], frq_2.get(str2[i]).concat(i))
         } else {
-            frq_1[str1[i]] = [i]
-            frq_2[str2[i]] = [i]
+            frq_1.set(str1[i], [i])
+            frq_2.set(str2[i], [i])
         }
     }
 
