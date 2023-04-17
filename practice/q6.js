95343e051ef921a4f97b9ce51edfcb3ad202074c
Igboanugwo Collins <munisco12@gmail.com>
2023-04-17 13:08:22 +0100

    working on algo

---
+++ practice/q6.js
@@ -0,0 +1,2 @@
+//Given two strings s and t, determine whether they are isomorphic, two strings s and t are isomorphic if the characters in s can be replaced to get t.
+//All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself. s and t consist of any valid ascii character
