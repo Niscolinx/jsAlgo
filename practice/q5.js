ebb47ab9ed5db413254d7ef56ccbe7b6e22d38c0
Igboanugwo Collins <munisco12@gmail.com>
2023-04-17 09:36:09 +0100

    working on algo

--- practice/q5.js
+++ practice/q5.js
@@ -1,1 +1,9 @@
 // You are given an array of integers and another target targetValue. Write a function that will take these inputs and return the indices of the 2 integers in the array that add up to targetValue
+
+
+// const arr1 = [3,5,3,6]
+// const targetValue = 8
+
+
+//Divide targetValue by 2
+// In the case of 8, it's 4 and 4
