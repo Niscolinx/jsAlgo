973948ba7e87a5d7245c6943e58f6fa2adb7d237
Igboanugwo Collins <munisco12@gmail.com>
2023-04-17 13:35:10 +0100

    working on algo

--- practice/q6.js
+++ practice/q6.js
@@ -1,35 +1,40 @@
 //Given two strings s and t, determine whether they are isomorphic, two strings s and t are isomorphic if the characters in s can be replaced to get t.
 //All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself. s and t consist of any valid ascii character.
 
+// {
+//     a:[0, 2],
+//     b:[1],
+//     c:[2],
+//     u:[3],
+//     s:[4]
+// }
+// {
+//     p:[0, 2],
+//     b:[1],
+//     g:[2],
+//     i:[3, 4]
+// }
 
-// const s = 'abacus'
-// const t = 'pbpgii'
+const isomorphic = (str1, str2) => {
+    const frq_1 = {}
+    const frq_2 = {}
 
-{
-    a:[0, 2],
-    b:[1],
-    c:[2],
-    u:[3],
-    s:[4]
-}
-{
-    p:[0, 2],
-    b:[1],
-    g:[2],
-    i:[3, 4]
-}
+    for (let i = 0; i < str1.length; i++) {
+        if (frq_1[str1[i]]) {
 
-const isomorphic = (string1, string2) => {
-    
-    const string1_frequency_count = {}
-    const string2_frequency_count = {}
+            frq_1[str1[i]] = frq_1[str1[i]].concat(i)
+            frq_2[str2[i]] = frq_2[str2[i]].concat(i)
 
-    for(let i = 0; i < string1.length; i++){
-
-        string1_frequency_count[string1] ??= [...string1_frequency_count[string1], i]
-        string1_frequency_count[string2] ??= [...string1_frequency_count[string2], i]
+        } else {
+            frq_1[str1[i]] = [i]
+            frq_2[str2[i]] = [i]
+        }
     }
 
-
-
+    console.log({ frq_1, frq_2 })
 }
+
+const s = 'abacus'
+const t = 'pbpgio'
+
+console.log(isomorphic(s, t))
