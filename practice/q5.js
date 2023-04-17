79cda3af05a7b7f7616ec7dd368d4f280e11fe8c
Igboanugwo Collins <munisco12@gmail.com>
2023-04-17 09:29:56 +0100

    working on algo

---
+++ practice/q5.js
@@ -0,0 +1,1 @@
+// You are given an array of integers and another target targetValue. Write a function that will take these inputs and return the indices of the 2 integers in the array that add up to targetValue
