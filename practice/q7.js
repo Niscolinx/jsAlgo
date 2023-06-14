e5c441f649014c4e008173056a5d6b7030f6f91f
Igboanugwo Collins <munisco12@gmail.com>
2023-06-14 19:22:16 +0100

    added hash table, and it made a lot of difference

--- practice/q7.js
+++ practice/q7.js
@@ -1,8 +1,8 @@
 //In the fibonacci sequence, each subsequent term is obtained by adding the first 2 preceding 2 terms. This is true for all numbers except the first 2 numbers of the fibonacci series, as they do not have 2 preceding numbers. The first 2 terms in the fibonacci sequence are 0 and 1. F(n) = F(n-1) + F(n-2) for n > 1. Write a function that finds F(n) given n, where n is an integer greater than or equal to 0. For the first term n = 0
 
 
+const hash = {}
 function fibonacci(n) {
-    const hash = {}
     if (n <= 1) return n
 
     const getTimeNow = Date.now()
