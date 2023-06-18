de576e28f8e46927bd9866767f9ea578afa3da7f
Igboanugwo Collins <munisco12@gmail.com>
2023-06-18 22:18:01 +0100

    solving algo

--- practice/q8.js
+++ practice/q8.js
@@ -1,10 +1,14 @@
 // [1, 3, [5,8]] = 1 + 3 + (5 * 8)^2
 // [3,5, [1,8, [9, 3], 2], 1] = 3 + 5 + (1 + 8 + (9 + 3) ^ 3 + 2)^2 + 1
 
-function peculiarArray (arr){
+function peculiarArray(arr) {
+    let total = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            
 
-    if (Array.isArray(arr)) {
-        
+        }
+
+        total += arr[i]
     }
-
 }
