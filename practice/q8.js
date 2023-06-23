505373571c88e31ba9b51895c1796728d5cf8777
Igboanugwo Collins <munisco12@gmail.com>
2023-06-23 21:23:57 +0100

    working on redis

--- practice/q8.js
+++ practice/q8.js
@@ -1,10 +1,21 @@
-// [1, 3, [5,8]] = 1 + 3 + (5 * 8)^2
+// [1, 3, [5,8]] = 1 + 3 + (5 + 8)^2
 // [3,5, [1,8, [9, 3], 2], 1] = 3 + 5 + (1 + 8 + (9 + 3) ^ 3 + 2)^2 + 1
 
 function peculiarArray(arr) {
-    let total = 0
-    
-    let count = 0
-
-    
+    function sum(arr, depth = 1) {
+        let total = 0
+        for (let i = 0; i < arr.length; i++) {
+            if (Array.isArray(arr[i])) {
+                depth++
+                total += Math.pow(sum(arr[i], depth), depth)
+                depth--
+            } else {
+                total += arr[i]
+            }
+        }
+        return total
+    }
+    return sum(arr, depth)
 }
+
+console.log(peculiarArray([1, 3, [5,8]]))
